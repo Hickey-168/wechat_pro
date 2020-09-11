9e4cc0e84e0f126fe0883f9c0a873ab16b0a9813
Hickey-168 <2361190278@qq.com>
2020-09-11 09:51:07 +0800

    commit changes

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,8 +11,8 @@ Page({
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+    wx.switchTab({
+      url: '../navi/navi'
     })
   },
   onLoad: function () {
